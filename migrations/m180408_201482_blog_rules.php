9fdf91f990b6bf716cd26375c20741992b310e3c
diazoxide <diazoxide@panteloma>
2019-03-09 11:10:44 +0400

    Updating Migrations

--- migrations/m180408_201482_blog_rules.php
+++ migrations/m180408_201482_blog_rules.php
@@ -20,7 +20,7 @@ use yii\db\Schema;
  * - `{{%blog_comment}}` -
  * - `{{%blog_tag}}` -
  */
-class m180408_201482_blog_init extends Migration
+class m180408_201482_blog_rules extends Migration
 {
     use \diazoxide\blog\traits\ModuleTrait;
 
@@ -127,124 +127,7 @@ class m180408_201482_blog_init extends Migration
      */
     public function up()
     {
-        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
 
-        if ($this->db->getTableSchema('{{%blog_category}}', true) === null) {
-            // MySql table options
-
-            // table blog_category
-            $this->createTable(
-                '{{%blog_category}}',
-                [
-                    'id' => Schema::TYPE_PK,
-                    'parent_id' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
-                    'title' => Schema::TYPE_STRING . '(255) NOT NULL',
-                    'slug' => Schema::TYPE_STRING . '(128) NOT NULL',
-                    'icon_class' => Schema::TYPE_STRING . '(255) DEFAULT NULL',
-                    'banner' => Schema::TYPE_STRING . '(255) ',
-                    'is_nav' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 1',
-                    'is_featured' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 1',
-                    'sort_order' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 50',
-                    'page_size' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 10',
-                    'template' => Schema::TYPE_STRING . '(255) NOT NULL DEFAULT "post"',
-                    'redirect_url' => Schema::TYPE_STRING . '(255) DEFAULT NULL',
-                    'status' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 1',
-                    'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
-                    'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL'
-                ],
-                $tableOptions
-            );
-
-            // Indexes
-            $this->createIndex('is_nav', '{{%blog_category}}', 'is_nav');
-            $this->createIndex('sort_order', '{{%blog_category}}', 'sort_order');
-            $this->createIndex('status', '{{%blog_category}}', 'status');
-            $this->createIndex('created_at', '{{%blog_category}}', 'created_at');
-
-        }
-
-        if ($this->db->getTableSchema('{{%blog_post}}', true) === null) {
-
-            // table blog_post
-            $this->createTable(
-                '{{%blog_post}}',
-                [
-                    'id' => Schema::TYPE_PK,
-                    'category_id' => Schema::TYPE_INTEGER . ' NOT NULL',
-                    'title' => Schema::TYPE_STRING . '(255) NOT NULL',
-                    'brief' => Schema::TYPE_TEXT,
-                    'content' => Schema::TYPE_TEXT . ' NOT NULL',
-                    'tags' => Schema::TYPE_STRING . '(255) NOT NULL',
-                    'slug' => Schema::TYPE_STRING . '(128) NOT NULL',
-                    'banner' => Schema::TYPE_STRING . '(255) ',
-                    'is_slide' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 1',
-                    'click' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0',
-                    'user_id' => Schema::TYPE_INTEGER . '',
-                    'status' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 1',
-                    'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
-                    'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL'
-                ],
-                $tableOptions
-            );
-
-            // Indexes
-            $this->createIndex('category_id', '{{%blog_post}}', 'category_id');
-            $this->createIndex('is_nav', '{{%blog_post}}', 'is_slide');
-            $this->createIndex('status', '{{%blog_post}}', 'status');
-            $this->createIndex('created_at', '{{%blog_post}}', 'created_at');
-
-            // Foreign Keys
-            $this->addForeignKey('{{%FK_post_category}}', '{{%blog_post}}', 'category_id', '{{%blog_category}}', 'id', 'CASCADE', 'CASCADE');
-            $userClass = \dektrium\user\models\User;
-            $userPK = 'id';
-            $this->addForeignKey('{{%FK_post_user}}', '{{%blog_post}}', 'user_id', $userClass::tableName(), $userPK, 'CASCADE', 'CASCADE');
-
-        }
-
-        if ($this->db->getTableSchema('{{%blog_comment}}', true) === null) {
-
-            // table blog_comment
-            $this->createTable(
-                '{{%blog_comment}}',
-                [
-                    'id' => Schema::TYPE_PK,
-                    'post_id' => Schema::TYPE_INTEGER . ' NOT NULL',
-                    'content' => Schema::TYPE_TEXT . ' NOT NULL',
-                    'author' => Schema::TYPE_STRING . '(128) NOT NULL',
-                    'email' => Schema::TYPE_STRING . '(128) NOT NULL',
-                    'url' => Schema::TYPE_STRING . '(128) NULL',
-                    'status' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 1',
-                    'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
-                    'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL'
-                ],
-                $tableOptions
-            );
-
-            // Indexes
-            $this->createIndex('post_id', '{{%blog_comment}}', 'post_id');
-            $this->createIndex('status', '{{%blog_comment}}', 'status');
-            $this->createIndex('created_at', '{{%blog_comment}}', 'created_at');
-
-            // Foreign Keys
-            $this->addForeignKey('{{%FK_comment_post}}', '{{%blog_comment}}', 'post_id', '{{%blog_post}}', 'id', 'CASCADE', 'CASCADE');
-        }
-
-        if ($this->db->getTableSchema('{{%blog_tag}}', true) === null) {
-
-            // table blog_tag
-            $this->createTable(
-                '{{%blog_tag}}',
-                [
-                    'id' => Schema::TYPE_PK,
-                    'name' => Schema::TYPE_STRING . '(128) NOT NULL',
-                    'frequency' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 1',
-                ],
-                $tableOptions
-            );
-
-            // Indexes
-            $this->createIndex('frequency', '{{%blog_tag}}', 'frequency');
-        }
 
         //$this->removeAllAuthItems();
 
@@ -331,9 +214,6 @@ class m180408_201482_blog_init extends Migration
      */
     public function down()
     {
-        $this->dropTable('{{%blog_tag}}');
-        $this->dropTable('{{%blog_comment}}');
-        $this->dropTable('{{%blog_post}}');
-        $this->dropTable('{{%blog_category}}');
+        
     }
 }
