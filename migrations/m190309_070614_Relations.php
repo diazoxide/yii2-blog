b3559d1f9fc7b41de542f759daa8ea17a8b29c15
diazoxide <diazoxide@panteloma>
2019-03-14 21:21:14 +0400

    Automatic Widgets

--- migrations/m190309_070614_Relations.php
+++ migrations/m190309_070614_Relations.php
@@ -14,16 +14,16 @@ class m190309_070614_Relations extends Migration
 
     public function safeUp()
     {
-        $this->addForeignKey('fk_blog_comment_post_id',
-            '{{%blog_comment}}', 'post_id',
-            '{{%blog_post}}', 'id',
-            'CASCADE', 'CASCADE'
-        );
-        $this->addForeignKey('fk_blog_post_category_id',
-            '{{%blog_post}}', 'category_id',
-            '{{%blog_category}}', 'id',
-            'CASCADE', 'CASCADE'
-        );
+//        $this->addForeignKey('fk_blog_comment_post_id',
+//            '{{%blog_comment}}', 'post_id',
+//            '{{%blog_post}}', 'id',
+//            'CASCADE', 'CASCADE'
+//        );
+//        $this->addForeignKey('fk_blog_post_category_id',
+//            '{{%blog_post}}', 'category_id',
+//            '{{%blog_category}}', 'id',
+//            'CASCADE', 'CASCADE'
+//        );
     }
 
     public function safeDown()
